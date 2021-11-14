5ec940bef60c0b44efaff3d59690b71fec92dfef
mattlockyer <mattdlockyer@gmail.com>
2021-11-14 07:38:02 +0000

    remove encode of redirect url for wallet signIn, bc. done in near-api-js

--- src/state/near.js
+++ src/state/near.js
@@ -24,7 +24,7 @@ export const initNear = () => async ({ update, getState, dispatch }) => {
 
 	wallet.signIn = () => {
 		console.log(contractId);
-		wallet.requestSignIn(contractId, 'Spearmint NFT API', encodeURIComponent(window.location.href));
+		wallet.requestSignIn(contractId, 'Spearmint NFT API', window.location.href);
 	};
 	const signOut = wallet.signOut;
 	wallet.signOut = () => {
